c3b6b60d20e55eaf68bdfd71437e73378bc03791
briheet01 <briheetyadav@gmail.com>
2024-04-24 23:41:08 +0530

    server and price

--- grpc_server.go
+++ grpc_server.go
@@ -1,12 +1,24 @@
 package main
 
-import "net"
+import (
+	"net"
 
-func makeGRPCServer(listenAddr string, svc PriceService) error {
+	"github.com/briheet/micro/proto"
+	"google.golang.org/grpc"
+)
+
+func makeGRPCServerandRun(listenAddr string, svc PriceService) error {
 	grpcPriceFetcher := NewGRPCPriceFetcherServer(svc)
 
 	ln, err := net.Listen("tcp", listenAddr)
 	if err != nil {
 		return err
 	}
+
+	opts := []grpc.ServerOption{}
+	server := grpc.NewServer(opts...)
+
+	proto.RegisterPriceFetcherServer(server, grpcPriceFetcher)
+
+	return server.Serve(ln)
 }
--- service.go
+++ service.go
@@ -1,6 +1,9 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 var prices = map[string]float64{
 	"ETH": 999.99,
@@ -10,4 +13,16 @@ var prices = map[string]float64{
 
 type PriceService interface {
 	FetchPrice(context.Context, string) (float64, error)
+}
+
+type priceService struct{}
+
+func (s *priceService) FetchPrice(c context.Context, ticker string) (float64, error) {
+	price, ok := prices[ticker]
+
+	if !ok {
+		return 0.0, fmt.Errorf("price for the ticker (%s) is not available", ticker)
+	}
+
+	return price, nil
 }
