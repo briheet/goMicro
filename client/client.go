9c3fea912a95e1efb778cacb777df130f3caabad
briheet01 <briheetyadav@gmail.com>
2024-04-24 16:02:59 +0530

    types, clientdef

--- client/client.go
+++ client/client.go
@@ -1,4 +1,17 @@
 package client
 
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
 func NewGRPCClient(remoteAddr string) (proto.PriceFetcherClient, error) {
+	conn, err := grpc.Dial(remoteAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	c := proto.NewPriceFetcherClient(conn)
+
+	return c, err
 }
